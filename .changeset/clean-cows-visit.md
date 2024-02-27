9fe080e4bd7524b1dde263d42a64a63ce4538c99
fwestling-lor <fwestling@laingorourke.com.au>
2024-02-27 15:21:47 +1100

    Updated vite config to include stats, and fixed CI/CD to do PNPM properly

---
+++ .changeset/clean-cows-visit.md
@@ -0,0 +1,5 @@
+---
+"fwestling-site": patch
+---
+
+Updated vite config to include stats, and fixed CI/CD to do PNPM properly
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from "vite";
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+
+    tsconfigPaths(),
+    react(),
+
+    svgr({
+      svgrOptions: {
+        svgo: true,
+      },
+    }),
+    visualizer() as PluginOption,
+    
+  ],
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        experimentalMinChunkSize: Infinity,
+        chunkFileNames: "chunk-[name].[hash].js",
+        entryFileNames: "entry-[name].[hash].js",
+      },
+    },
+  },
 })
