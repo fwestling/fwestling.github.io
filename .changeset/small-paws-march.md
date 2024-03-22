0260bb64683dc55cf9c152671d5a529acbbf65c7
Fred Westling <fred@westling.io>
2024-03-23 08:26:12 +1100

    :construction_worker: ci: Added firebase hosting

---
+++ .changeset/small-paws-march.md
@@ -0,0 +1,5 @@
+---
+"fwestling-site": patch
+---
+
+Added firebase
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,23 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// These are safe to store in code, as per Firebase's security settings.
+const firebaseConfig = {
+	apiKey: "AIzaSyAU7AVIVGQVfT_lX5cteSWvjHaUh3Lp0hs",
+	authDomain: "westling-website.firebaseapp.com",
+	projectId: "westling-website",
+	storageBucket: "westling-website.appspot.com",
+	messagingSenderId: "1064264798875",
+	appId: "1:1064264798875:web:1be857b60a538b62dc6c3e",
+	measurementId: "G-KVRVV2Q9KB",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { ChakraProvider } from "@chakra-ui/react";
